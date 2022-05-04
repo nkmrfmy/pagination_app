cbcadb2a0160f4aa19bcd2b825f2a452ebd0dc63
FMY <n.fmy.0526@icloud.com>
2022-05-04 15:06:57 +0900

    ページネーションを実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  PER_PAGE = 10
+
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(PER_PAGE)
   end
 
   def create
