b40b63e69485863ef6ecb4b215df74c81dcc42d2
FMY <n.fmy.0526@icloud.com>
2022-05-04 14:59:26 +0900

    bodyカラムに空文字が保存できないようバリデーションを追加,Fakerで初期データを追加,_post.html.erbを作成し一覧表示でレンダリング

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+Faker::Config.locale = :ja
+50.times do
+  Post.create!(body: Faker::Name.name)
+end
